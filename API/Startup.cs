b05c558bafb07a49ec96fb9057a1eb3c64d6a052
SaifUrRehman786 <safe4515@gmail.com>
2022-05-28 07:25:35 +0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,12 +31,14 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            
             services.AddSwaggerGen();
             services.AddDbContext<DataContext>(options =>{
 
                  options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +54,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
